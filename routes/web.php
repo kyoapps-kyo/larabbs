22365269f682896cab468f9fe9836897f33cc59b
Kyo <kyo@kyoapps.com>
2021-09-10 06:01:57 +0000

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,6 @@ Auth::routes();
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+//用户控制资源路由
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
