d3f925a1a5b1af77b2987eba7c56373adb24481f
Kyo <kyo@kyoapps.com>
2021-12-05 15:31:40 +0000

    本地化

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -8,4 +8,8 @@ use App\Http\Controllers\Controller as BaseController;
 class Controller extends BaseController
 {
     //
+    public function errorResponse($statusCode, $message=null, $code=0)
+    {
+        throw new HttpException($statusCode, $message, null, [], $code);
+    }
 }
